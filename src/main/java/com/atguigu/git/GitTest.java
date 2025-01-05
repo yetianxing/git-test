4ea7e3909c4e7146648da55401902edecc9484e4
Yetianxing <2859151581@qq.com>
2025-01-05 11:35:02 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author 灵药
+ * @version 1.0
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
